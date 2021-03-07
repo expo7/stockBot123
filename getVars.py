494841616f55db2a77ad9b2ecc986082ae73adec
expo7 <bsavelli66@gmail.com>
2021-03-07 13:04:23 -0700

    add data

--- getVars.py
+++ getVars.py
@@ -3,4 +3,3 @@ BINANCE_KEY=os.environ.get('BINANCE_API_KEY')
 BINANCE_SECRET=os.environ.get('BINANCE_API_SECRET')
 GEMINI_KEY=os.environ.get('BINANCE_API_KEY')
 GEMINI_SECRET=os.environ.get('BINANCE_API_SECRET')
-
